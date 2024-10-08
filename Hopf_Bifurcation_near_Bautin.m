bbc085d2d11544753093b67a8f8d332afd5792a1
AAlhadbani <aa1366@exeter.ac.uk>
2024-10-08 11:17:53 +0100

    Update Hopf_Bifurcation_near_Bautin.m

--- Hopf_Bifurcation_near_Bautin.m
+++ Hopf_Bifurcation_near_Bautin.m
@@ -6,74 +6,72 @@ alpha = -1:0.01:1; % alpha values
  % parameter 
  beta=1;
  %sigma=-1
-%arrays
-rstable = NaN(size(alpha)); % to store stable limit cycle values
-runstable = NaN(size(alpha)); % to store unstable limit cycle values
+%arrays 
+%  limit cycle values (stable and unstable)
+rstable = NaN(size(alpha));
+runstable = NaN(size(alpha));
 
-%alpha and compute limit cycles
-for mu = 1:length(alpha) %mu is an integer not parameter
-    if alpha(mu) <= 0 && beta>0
-        % Subcritical  
+%loop to compute limit cycles
+for mu = 1:length(alpha) %mu is an integer, is not parameter but alpha is a parameter
+    if alpha(mu) <= 0 &&  beta>0 % the first case when alpha<= 0 and  beta>0 (Subcritical hopf bifurcation)
         runstable(mu) = sqrt(-alpha(mu)/beta);
     end
-    if alpha(mu) >= 0 && beta<0
-        % SuPercritical  
+    if alpha(mu) >= 0 && beta<0  %the second case when alpha>= 0 and  beta<0 ( Supercritical hopf bifurcation)
         rstable(mu) = sqrt(-alpha(mu)/beta);
     end
 end
 
-% Plot bifurcation diagram
+% To plot bifurcation diagram
 figure(1); clf; 
-if beta<0
-plot(alpha(~isnan(rstable)), rstable(~isnan(rstable)), 'r-', 'LineWidth',2,'DisplayName','stable po');
+if beta<0 %first case
+plot(alpha(~isnan(rstable)), rstable(~isnan(rstable)), 'r-', 'LineWidth',2,'DisplayName','stable p.o.');
 end
 hold on;
-if beta>0
-plot(alpha, runstable, 'r--', 'LineWidth', 2,'DisplayName','unstable po');
+if beta>0 %second case
+plot(alpha, runstable, 'r--', 'LineWidth', 2,'DisplayName','unstable p.o.');
 end
 xlabel('\alpha');
 ylabel('r' );
 xlim([-1,1])
 legend();
 grid on;
-plot(alpha(alpha<0),0*alpha(alpha<0),'g-','LineWidth', 2,'DisplayName','stable eq')
-plot(alpha(alpha>0),0*alpha(alpha>0),'b--','LineWidth', 2,'DisplayName','unstable eq')
+plot(alpha(alpha<0),0*alpha(alpha<0),'g-','LineWidth', 2,'DisplayName','E_s')
+plot(alpha(alpha>0),0*alpha(alpha>0),'b--','LineWidth', 2,'DisplayName','E_u')
 %%
 clc
 clear
-%Hopf Bifurcation near Bautin
-% the differential equation r'=alpha(\mu)*r+\beta*r^3 + sigma* r^5, where sigma=-1
+%Hopf Bifurcation near Bautin (generalized Hopf bifurcation.)
+% the differential equation r'=alpha(\mu)*r+\beta*r^3 + sigma* r^5, where sigma=-1 ,where alpha and beta are parameters 
 alpha = -1:0.001:1; %  values of alpha
- % parameter 
- beta=-1;
+ beta=-1; % we have 2 cases: figure(1) when beta=1 and figure(2) when beta=-1 
 %r_stable and r_unstable are arrays 
-r_stable = NaN(length(alpha),1); % to store stable limit cycle values
-r_unstable = NaN(length(alpha),1); % to store unstable limit cycle values
+r_stable = NaN(length(alpha),1); %  limit cycle values(stable)
+r_unstable = NaN(length(alpha),1); % unstable limit cycle values (unstable)
 %loop to find limit cycles (period)
 for mu = 1:length(alpha) % mu is an integer not parameter
-    if alpha(mu) <= 0 && beta>0 && -4*alpha(mu)<=beta^2 % First condition
+    if alpha(mu) <= 0 && beta>0 && -4*alpha(mu)<=beta^2 % First case 
         A=beta^2 + 4*alpha(mu); % Quadratic Equation
         a = sqrt((beta - sqrt(A)) / 2);   
         b= sqrt((beta + sqrt(A)) / 2);
         r_stable(mu)= b;
         r_unstable(mu)= a;
     end
-    if  alpha(mu) >= 0 && beta>0  % Second condition
+    if  alpha(mu) >= 0 && beta>0  % Second case
          A=beta^2 + 4*alpha(mu);
         b=sqrt((beta + sqrt(A)) / 2);
         r_stable(mu)= b;
     end    
-      if  alpha(mu) >= 0 && beta<0 
+      if  alpha(mu) >= 0 && beta<0  % third case, we have fourth case but no real solution 
           A=beta^2 + 4*alpha(mu);
          b= sqrt((beta + sqrt(A)) / 2);
          r_stable(mu)= b;
      end  
 end
 %%
-% Plot bifurcation diagram
-if beta>0 
+% To plot the bifurcation diagram
+if beta>0  % first case 
     figure(1); clf; 
-else
+else  %other case
     figure(2); clf;
 end
  plot(alpha,r_stable, 'r-', 'LineWidth',2,'DisplayName','stable p.o.');
