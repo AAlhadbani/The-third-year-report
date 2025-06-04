dced9f566d3f743e61bb07240475fb79d5a8b9c7
AAlhadbani <aa1366@exeter.ac.uk>
2025-06-04 21:13:27 +0100

    Update DDEsexample.m

--- DDEsexample.m
+++ DDEsexample.m
@@ -1,8 +1,8 @@
 %Hopf bifurcation for DDEs example (Third year report)
 syms t r
-tau=sym(pi/2);
-m=3; % change as you want
-B=[1,reshape([cos((1:m)*t);sin((1:m)*t)],1,[])]; %B(t)
+tau=sym(pi/2); %value of tau
+m=3; % changing 
+B=-1*[1,reshape([cos((1:m)*t);sin((1:m)*t)],1,[])]; %B(t)
 Bp=diag([1,2*ones(1,2*m)])*B.';  %B^+(t)
 Bdel=subs(B,t,t-tau);   %B(t-tau)
 C=int(Bp*Bdel,[0,2*pi])/(2*pi);
