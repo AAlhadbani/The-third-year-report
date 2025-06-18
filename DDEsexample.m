7b04937ec9fc4ba8128b55585c301aed83fd2435
AAlhadbani <aa1366@exeter.ac.uk>
2025-06-18 16:27:01 +0100

    Update DDEsexample.m

--- DDEsexample.m
+++ DDEsexample.m
@@ -1,19 +1,19 @@
 %Hopf bifurcation for DDEs example (Third year report)
-syms t r a
+clear
+syms t r a om
 tau=sym(pi/2);
-m=2; % change as you want
-B=[1,reshape([cos((1:m)*t);sin((1:m)*t)],1,[])]; %B(t);
+om0=1;
+m=2;
+B=[1,reshape([cos((1:m)*t);sin((1:m)*t)],1,[])]; %B(t)
 Bp=diag([1,2*ones(1,2*m)])*B.';  %B^+(t)
-Bdel=subs(B,t,t-tau); %B(t-tau)
-gx=subs(-B,t,t-a);
-gx0=subs(gx,a,tau);
-A=Bp*(gx0);
-C0=int(A,[0,2*pi])/(2*pi);
+gtilde=subs(-B,t,t-om*a);   %B(t-tau)
+gt0=subs(gtilde,[a,om],[tau,om0]);   %B(t-tau)
+C0=int(Bp*gt0,[0,2*pi])/(2*pi);
 R0p=blkdiag(0,kron(diag(1:m),[0,-1;1,0])); %R'(0)
-F0=R0p+C0;
+F0=om0*R0p+C0
 V=null(F0); %right nullspace
 W=null(F0'); %left nullspace
-dgxa=diff(gx,a);
-dgxa0=subs(dgxa,a,tau);
-dgxa1=int(Bp*dgxa0,[0,2*pi])/(2*pi);
-W'*dgxa1*V(:,1)
+intgt=int(Bp*gtilde,[0,2*pi])/(2*pi); %taktng integral
+da=subs(diff(om*R0p+intgt,a),[a,om],[tau,om0]);
+do=subs(diff(om*R0p+intgt,om),[a,om],[tau,om0]);
+[W'*do*V(:,1),W'*da*V(:,1)] %matrix
