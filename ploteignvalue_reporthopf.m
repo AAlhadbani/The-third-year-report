70c1dab87d4d31c5187de058edd34c973456be8d
AAlhadbani <aa1366@exeter.ac.uk>
2026-01-18 15:29:20 +0000

    Refactor stability analysis with new delay variable
    .

--- ploteignvalue_reporthopf.m
+++ ploteignvalue_reporthopf.m
@@ -2,8 +2,8 @@ clc
 clear
 A=0; % First
 B=-1;  % Second
-mu=1.2 % delay mu
-    stability=dde_stst_eig_cheb(cat(3,A,B),mu,'max_number_of_eigenvalues',50,'minimal_real_part',-4,'maxsize',500);
+a=pi/4 % delay mu
+    stability=dde_stst_eig_cheb(cat(3,A,B),a,'max_number_of_eigenvalues',50,'minimal_real_part',-4,'maxsize',500);
     ev=stability.l0;
 
 figure(1);clf
@@ -12,33 +12,34 @@ nexttile
 plot(real(ev),imag(ev),'o','LineWidth',2); % Plot linear stability analysis
 grid on
 xline(0,'LineWidth',2)
-xlim([-3,1])
+xlim([-5,1])
 ylim([-12,12])
-title('\mu=1.2')
+title('$a=\frac{\pi}{4}$','Interpreter','latex')
 xlabel('Re $\lambda$',Interpreter='latex')
 ylabel('Im $\lambda$',Interpreter='latex')
 nexttile
-mu=pi/2 % delay mu
-    stability=dde_stst_eig_cheb(cat(3,A,B),mu,'max_number_of_eigenvalues',50,'minimal_real_part',-4,'maxsize',500);
+a=pi/2 % delay mu
+    stability=dde_stst_eig_cheb(cat(3,A,B),a,'max_number_of_eigenvalues',50,'minimal_real_part',-4,'maxsize',500);
     ev=stability.l0;
     plot(real(ev),imag(ev),'o','LineWidth',2); % Plot linear stability analysis
 grid on
 xline(0,'LineWidth',2)
 xlim([-2.5,1])
 ylim([-12,12])
-title('\mu=1.5708')
+title('$a=\frac{\pi}{2}$','Interpreter','latex')
 xlabel('Re $\lambda$',Interpreter='latex')
 ylabel('Im $\lambda$',Interpreter='latex')
 nexttile
-mu=1.8 % delay mu
-    stability=dde_stst_eig_cheb(cat(3,A,B),mu,'max_number_of_eigenvalues',50,'minimal_real_part',-4,'maxsize',500);
+a=(3*pi)/2 % delay mu
+    stability=dde_stst_eig_cheb(cat(3,A,B),a,'max_number_of_eigenvalues',50,'minimal_real_part',-4,'maxsize',500);
     ev=stability.l0;
     plot(real(ev),imag(ev),'o','LineWidth',2); % Plot linear stability analysis
 grid on
 xline(0,'LineWidth',2)
 xlim([-2.5,1])
 ylim([-12,12])
-title('\mu=1.8')
+title('$a=\frac{3\pi}{2}$','Interpreter','latex')
 xlabel('Re $\lambda$',Interpreter='latex')
 ylabel('Im $\lambda$',Interpreter='latex')
  exportgraphics(figure(1),'the eigenvalue hopf bif report hopf.png')
+ set(gcf, 'Position', [100, 100, 1100, 400]); % Set the figure size
